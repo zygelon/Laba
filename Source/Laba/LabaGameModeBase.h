f392f6a500e33c39203791479ab2555cb114bbff
zygelon <zygelon@gmail.com>
2019-04-11 08:47:39 +0300

    Start Developing

---
+++ Source/Laba/LabaGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LabaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABA_API ALabaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
